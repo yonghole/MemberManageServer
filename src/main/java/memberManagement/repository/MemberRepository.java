bb03c1c1fa3bf2d1eb194cf73699a099861a1887
YongSangHo <yonghole@ajou.ac.kr>
2021-03-12 23:30:19 +0900

    Spring practice Proj.

---
+++ src/main/java/memberManagement/repository/MemberRepository.java
@@ -0,0 +1,16 @@
+package memberManagement.repository;
+
+import memberManagement.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+
+    // Must implemented Functions
+
+    Member save(Member member);
+    Optional<Member> findByID(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
